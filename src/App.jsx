a962abd6410d1814155274af171ffb83672cbc49
Zerio <54480523+Z3rio@users.noreply.github.com>
2022-10-15 02:43:31 +0200

    Setup basic header and idea

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  function toggleDarkTheme() {
+    setDarkTheme(!darkTheme);
+    console.log(darkTheme);
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={`App ${darkTheme == true ? "darkmode" : "lightmode"}`}>
+      <div className="header">
+        <h1>Notes</h1>
+        <span className="mdi mdi-brightness-4" onClick={toggleDarkTheme} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="search">
+        <span className="mdi mdi-magnify" onClick={toggleDarkTheme} />
+        <input type="text" id="search" placeholder="Search" />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
